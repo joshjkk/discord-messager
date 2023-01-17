a503a52afc6cbf60462253a0c43339807de778c9
Joshua Kinder <joshjkk0@gmail.com>
2023-01-17 17:24:19 +0000

    Add UI and update to 2.0.0

--- README.md
+++ README.md
@@ -1,29 +1,22 @@
 # Discord Messager
 
-### Automatically send Discord Messages.
+### Automatically send Discord messages.
 
 ![GitHub file size in bytes](https://img.shields.io/github/size/joshjkk/discord-messager/script.py) ![GitHub](https://img.shields.io/github/license/joshjkk/discord-messager) ![GitHub release (latest by date)](https://img.shields.io/github/v/release/joshjkk/discord-messager)
 
 It's always fun to mess with your friends or a server, and now you can do it automatically.
 
-## Prerequisites
-
-* Python - Download from the [official website](https://www.python.org/) or [via terminal](https://docs.python-guide.org/starting/install3/linux/).
-* Python requests - Usually comes bundled with the Python installation, but you may need to [install it manually](https://pypi.org/project/requests/).
-
 ## Usage
 
 > *Discord does not allow [self-bots](https://support.discord.com/hc/en-us/articles/115002192352-Automated-user-accounts-self-bots-), by using this software, you are aware of possible account termination.*
 
-Firstly, you must fill out the parameters inside [script.py](./script.py) in order for it to work. Here are some resources to help you fill out the parameters:
+All you need to do is fill out the entries in the app then press ```Send Message```.
 
 ### Useful Resources
 
 * [How to Find Your Discord Token](https://www.youtube.com/watch?v=YEgFvgg7ZPI)
 * [How to Get Server ID, Channel ID, User ID in Discord](https://www.youtube.com/watch?v=NLWtSHWKbAI)
 * [Using Python Requests to Send Discord Messages](https://youtu.be/DArlLAq56Mo?t=56)
-
-After that, simply double click the Python file and it should start working. **Press <kbd>CTRL + C</kbd> or close the window at any time to stop the script.** 
 
 ## License
 
--- script.py
+++ script.py
@@ -1,4 +1,6 @@
+import tkinter as tk
 import requests
+import threading
 import time
 
 
@@ -7,44 +9,130 @@ import time
 # Channel link
 # LOOKS SOMETHING LIKE THIS:
 # https://discord.com/api/vNUMBER/SOME_NUMBERS/messages
-channel = r"LINK TO THE CHANNEL"
 
-# Message to send
-payload = {
-    'content': "MESSAGE TO SEND"
-}
-
-# Authorization
-headers = {
-    'authorization': "AUTHORIZATION KEY"
-}
-
-timeout = 30 # Timeout length in seconds
 
 ########## FILL THESE PARAMETERS IN ##########
 
 
+def notify_usage() -> None:
+    print("\nDiscord does not allow self-bots, by using this software you are aware of possible account termination.")
+    print("See https://support.discord.com/hc/en-us/articles/115002192352-Automated-user-accounts-self-bots- for info on self-bots.\n")
 
-def send_message() -> None:
-    # Send message
-    requests.post(url=channel, data=payload, headers=headers)
+
+class App:
+    def __init__(self, title: str, width: int, height: int, channel: str, message: str, authorization: str, timeout: int) -> None:
+        # Set up parameters
+        self.title = title
+        self.width = width
+        self.height = height
+
+        # POST parameters
+        self.channel = channel
+        self.message = message
+        self.authorization = authorization
+        self.timeout = timeout
+
+        # Create the window
+        self.version = "2.0.0"
+        self.root = tk.Tk()
+        self.root.title(title)
+        self.root.geometry(f"{width}x{height}+0+0")
+        self.root.resizable(width=False, height=False)
+
+        # Version number in the top left
+        self.version_num = tk.Label(self.root, text=f"v{self.version}", font=("Arial", 11))
+        self.version_num.place(relx=0.03, rely=0.02, anchor=tk.CENTER)
+
+        # Channel link entry
+        self.title_font = ("Arial", 13)
+        self.entry_font = ("Arial", 12)
+        
+        self.channel_title = tk.Label(self.root, text="Channel Link", font=self.title_font)
+        self.channel_title.place(relx=0.5, rely=0.065, anchor=tk.CENTER)
+
+        self.channel_entry = tk.Entry(self.root, width=60, font=self.entry_font)
+        self.channel_entry.place(relx=0.5, rely=0.15, anchor=tk.CENTER)
+
+        # Message entry
+        self.message_title = tk.Label(self.root, text="Message", font=self.title_font)
+        self.message_title.place(relx=0.5, rely=0.25, anchor=tk.CENTER)
+
+        self.message_entry = tk.Text(self.root, width=60, height=2, font=self.entry_font)
+        self.message_entry.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
+
+        # Authorization entry
+        self.authorization_title = tk.Label(self.root, text="Authorization Key", font=self.title_font)
+        self.authorization_title.place(relx=0.5, rely=0.475, anchor=tk.CENTER)
+
+        self.authorization_entry = tk.Entry(self.root, width=60, font=self.entry_font)
+        self.authorization_entry.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
+
+        # Timeout entry
+        self.timeout_title = tk.Label(self.root, text="Timeout Length (s)", font=self.title_font)
+        self.timeout_title.place(relx=0.5, rely=0.65, anchor=tk.CENTER)
+
+        self.timeout_entry = tk.Entry(self.root, width=3, font=self.entry_font)
+        self.timeout_entry.place(relx=0.5, rely=0.725, anchor=tk.CENTER)
+
+        # Send message button
+        self.button_font = ("Arial", 13)
+        self.send_button = tk.Button(self.root, text="Send Message", font=self.button_font, command=self.send_message)
+        self.send_button.place(relx=0.4, rely=0.9, anchor=tk.CENTER)
+
+        # Loop checkbox
+        self.auto = tk.BooleanVar()
+        self.loop_box = tk.Checkbutton(self.root, text="Loop", font=self.button_font, variable=self.auto, command=self.toggle_loop)
+        self.loop_box.place(relx=0.6, rely=0.9, anchor=tk.CENTER)
+
+        self.root.mainloop()
+
+
+    def toggle_loop(self) -> None:
+        if self.auto.get() == True:
+            self.loop_box.select()
+        else:
+            self.loop_box.deselect()
+
+
+    def send_message(self) -> None:
+        if self.auto.get() == False:
+            thread = threading.Thread(target=self.post, daemon=False)
+            thread.start()
+        else:
+            while self.auto.get() == True:
+                thread = threading.Thread(target=self.post, daemon=True)
+                thread.start()
+                wait = threading.Thread(target=lambda: time.sleep(self.timeout_entry.get()), daemon=False)
+                wait.start()
+                wait.join()
+
+
+    def post(self) -> None:
+        # POST parameters
+        payload = {
+            'content': self.message_entry.get(1.0, tk.END)
+        }
+
+        headers = {
+            'authorization': self.authorization_entry.get()
+        }
+
+        requests.post(url=self.channel_entry.get(), data=payload, headers=headers)
+
+
+"""
+self.channel = "CHANNEL TO SEND TO"
+self.message = "MESSAGE TO SEND"
+self.authorization = "AUTHORIZATION KEY"
+self.timeout = 30
+"""
 
 
 def main() -> int:
-    print("Discord does not allow self-bots, by using this software, you are aware of possible account termination.")
-    print("See https://support.discord.com/hc/en-us/articles/115002192352-Automated-user-accounts-self-bots- for support on self-bots.")
+    notify_usage()
+    App("Discord Messager", 675, 400, "fdsafd", "dffdaf", "dflkjdf", 30)
+    return 0
 
-    try:
-        while True:
-            # Send message and wait [timeout] seconds
-            send_message()
-            time.sleep(timeout)
-
-        return 0
-
-    except Exception as e:
-        print("Error: ", e)
-        return 1
 
 if __name__ == "__main__":
     exit(main())
